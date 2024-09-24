e94690854600c8aa95336ee89452a0781c09cba0
Matteo Contrini <matteo@contrini.it>
2024-09-24 22:44:34 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,12 +11,6 @@
 
 </div><!-- #main .site-main -->
 
-<footer id="colophon" class="site-footer" itemscope="itemscope" itemtype="http://schema.org/WPFooter" role="contentinfo">
-	<div class="site-info">
-		<?php echo independent_publisher_footer_credits(); ?>
-	</div>
-	<!-- .site-info -->
-</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
 
 <?php wp_footer(); ?>
